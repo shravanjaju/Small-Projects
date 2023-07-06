60acd16cfdb1ffc0e8141e8da4b0587a1f40ad2a
shravanjaju <shravanjaju77@gmail.com>
2023-07-06 11:41:45 +0530

    Multiplication Table

---
+++ src/Components/Tables.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+function Table() {
+
+const [number, gettbl] = useState('');
+
+const rows = [];
+    for(let i = 1; i <= 10; i++)
+    {
+    rows.push(
+        <tr key={i}> 
+        <td>{number} * {i} =  {i * number }</td>
+       
+        </tr>
+    );
+    }
+
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+     
+const getTable = (event) => {
+    gettbl(event.target.value); // for updating the value
+}
+
+  return (
+    <div>
+<form>
+    <label>Enter a Number :-</label>
+    <input type="number" placeholder="Enter any Number" 
+    onChange={getTable }
+    name="number"/> 
+  <div>
+  <p>{rows}</p>
+  </div>
+</form>
+ 
+</div>
+);
+}
+   
+export default Table
